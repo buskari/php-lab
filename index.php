534a13feb03f52e70a56afb6fb51bccc990f1e79
Andris Buscariolli <andris.contact@gmail.com>
2020-05-11 14:37:06 -0300

    1. Types of concatenation 	2. Escape "" double quotes 	3. Some string methods (strlen(), strtoupper(), strtolower(), str_repace())

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php 
 
-    define('NAME', 'Yoshi'); // this is how a constant is declared
-    // $name = 'Yoshi';
-    $age = 30;
+    $stringOne = 'my email is ';
+    $stringTwo = 'mario123@gmail.com';
+
+    // echo $stringOne.$stringTwo // This is how things are concatenated
+    
+    $name = 'Mario';
+
+    // echo 'My name is '.$mario;
+    
+    // differences between simple and double quotes
+    // echo "Hey my name is $mario"; // double reads variable
+    // echo 'Hey my name is $mario'; // simple reads literal
+
+    // echo "someone screamed \"whaaa\""; // this is how to make "" appears on a string
+    // echo 'someone screamed "whaaa"'; // another way to do the same (easier)
+
+    // echo $name[0];
+
+    // echo strlen($name); //function that shows the length of a string
+    // echo strtoupper($name);
+    // echo strtolower($name);
+    echo str_replace('M', 'W', $name);
 
 ?>
 
@@ -19,10 +38,6 @@
     </style>
 </head>
 <body>
-    <h1>User Profile Page</h1>
-    <div>
-        <?php echo NAME; ?>
-        <?php echo $age; ?>
-    </div>
+
 </body>
 </html>
